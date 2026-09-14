2d6fd8ef4b52fd7bfa7cdea2dd52fc0ad310e1ba
Lenindavid9 <idarragalenindavid@gmail.com>
2026-09-14 18:53:55 -0500

    wip: cambios locales antes de actualizar

--- lib/conf/conf.php
+++ lib/conf/conf.php
@@ -5,7 +5,7 @@
     // ejecutar BD_Dengue_SIGuppy.sql.
     $host     = "127.0.0.1";
     $user     = "postgres";
-    $password = "";         
+    $password = "lenin2007";          // <-- pon aquí la contraseña de tu postgres
     $database = "BD_Dengue_SIGuppy";
     $port     = "5432";
 
