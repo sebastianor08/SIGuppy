6fc9cb6f7e6b318527ae772853b0d503c1294eb3
jaidermontano69-netizen <jaidermontano69@gmail.com>
2026-09-17 14:25:59 -0500

    Correciones en todo

--- View/Reportes/ActividadesDeTerrenoPorTipoView.php
+++ View/Reportes/ActividadesDeTerrenoPorTipoView.php
@@ -24,11 +24,7 @@ include '../partials/head.php';
                         <div class="fila-filtros">
                             <div>
                                 <label>Tipo de actividad</label>
-<<<<<<< HEAD
-                                <select name="tipo">
-=======
                                 <select name="tipo" class="form-select">
->>>>>>> origin/dev
                                     <option value="">Todos</option>
                                     <?php foreach ($listaTipos as $tipo): ?>
                                         <option value="<?= $tipo ?>" <?= $filtroTipo === $tipo ? 'selected' : '' ?>>
@@ -39,11 +35,7 @@ include '../partials/head.php';
                             </div>
                             <div>
                                 <label>Zoocriadero</label>
-<<<<<<< HEAD
-                                <select name="zoocriadero">
-=======
                                 <select name="zoocriadero" class="form-select">
->>>>>>> origin/dev
                                     <option value="">Todas</option>
                                     <?php foreach ($listaZoocriaderos as $zoo): ?>
                                         <option value="<?= $zoo ?>" <?= $filtroZoocriadero === $zoo ? 'selected' : '' ?>>
@@ -54,19 +46,11 @@ include '../partials/head.php';
                             </div>
                             <div>
                                 <label>Fecha Inicio</label>
-<<<<<<< HEAD
-                                <input type="date" name="fecha_inicio" value="<?= $filtroFechaInicio ?>">
-                            </div>
-                            <div>
-                                <label>Fecha Fin</label>
-                                <input type="date" name="fecha_fin" value="<?= $filtroFechaFin ?>">
-=======
                                 <input type="date" name="fecha_inicio" class="form-control" value="<?= $filtroFechaInicio ?>">
                             </div>
                             <div>
                                 <label>Fecha Fin</label>
                                 <input type="date" name="fecha_fin" class="form-control" value="<?= $filtroFechaFin ?>">
->>>>>>> origin/dev
                             </div>
                             <div>
                                 <button type="submit" class="btn-aplicar">Aplicar Filtros</button>
